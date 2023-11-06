eef1a08c74dfd1c744ce931e99343fbca23b5f0b
Medranomiler1 <medranomiler@gmail.com>
2023-11-06 13:44:38 -0600

    fixed typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
     >
       <div className="h-1/4 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b  pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 lg:bg-zinc-800/30">
-          Get started by clicking entering an npub
+          Get started by entering a nostr npub
         </p>
 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t  from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
