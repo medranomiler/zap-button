22d45a65940e0f328a909b629c9848b81b283c29
Medranomiler1 <medranomiler@gmail.com>
2023-11-06 10:31:30 -0600

    removed header and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,24 +9,8 @@ export default function Home() {
 
   return (
     <main
-      className={`flex h-screen flex-col items-center justify-start p-24 ${inter.className}`}
+      className={`flex h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
-      <div className="h-1/4 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b  pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 lg:bg-zinc-800/30">
-          Get started by clicking the Zap Button
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t  from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://primal.net/p/npub1t4s73mnwahenrl3l083mfj3hr5jgd0f8twauxhdsc0v0q0u5478sxek2x9"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            @BTCRETRIEVER
-          </a>
-        </div>
-      </div>
       <div className="flex flex-col items-center w-full justify-center">
       <div className="flex flex-col items-center w-full justify-center mb-4">
       <Image src="/pleblabIcon.jpeg" height="100" width="100" className="rounded-full mb-4"/>
